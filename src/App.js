34fdc4602ffe20318e852ec53e2e2d48dc514a4a
ElizabethCDWolfe <elizabeth.wolfe11@gmail.com>
2024-08-17 16:09:28 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import React from 'react';
-import './App.css';
+import './App.css';  
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Welcome to my react web app
-        </p>
-        <a
-          className="App-link"
-          //href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
+        <h1>Welcome to My React App!</h1>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
 }
 
 export default App;
+
