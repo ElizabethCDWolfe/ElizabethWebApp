8ee95f9610fd8ae4503026b0e05510d8161b61ed
ElizabethCDWolfe <elizabeth.wolfe11@gmail.com>
2024-08-17 15:57:46 -0600

    Updates

---
+++ precache-manifest.62a92c1c35ce4e7cbe1c76d0b64e3a3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b24edcff3c65db4440fbc8b842a5d5",
+    "url": "/ElizabethWebApp/index.html"
+  },
+  {
+    "revision": "f8966fc1edc537cfb566",
+    "url": "/ElizabethWebApp/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "a6db8470c5eb6c2fbac9",
+    "url": "/ElizabethWebApp/static/js/2.4ccafed9.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/ElizabethWebApp/static/js/2.4ccafed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b31b59d54ac7bf184f",
+    "url": "/ElizabethWebApp/static/js/3.e969467f.chunk.js"
+  },
+  {
+    "revision": "f8966fc1edc537cfb566",
+    "url": "/ElizabethWebApp/static/js/main.5eb4c8ae.chunk.js"
+  },
+  {
+    "revision": "9564a6ab3171dd33898e",
+    "url": "/ElizabethWebApp/static/js/runtime-main.9139ff4c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ElizabethWebApp/static/media/logo.06e73328.svg"
+  }
+]);
